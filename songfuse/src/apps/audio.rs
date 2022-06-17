0fc15445c45037d4a03b974cbe74e6ba47352ff6
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2022-06-16 21:22:40 -0400

    Either save as decrypted clt or decoded wav

--- songfuse/src/apps/audio.rs
+++ songfuse/src/apps/audio.rs
@@ -3,6 +3,11 @@ use bfforever::audio::*;
 use clap::Parser;
 use log::debug;
 
+const SUPPORTED_EXTS: [&'static str; 1] = [
+    ".wav",
+    //".ogg",
+];
+
 #[derive(Parser)]
 pub struct AudioApp {
     #[clap(help = "Path to input audio file (clt/wav)", required = true)]
@@ -22,7 +27,18 @@ impl SubApp for AudioApp {
         let mut celt_audio = Celt::open(&self.input_path);
         celt_audio.decrypt();
 
-        celt_audio.save(&self.output_path);
+        if self.input_path.ends_with(".clt") {
+            // Save as decrypted .clt
+            celt_audio.save(&self.output_path);
+        } else {
+            // Decode and save as .wav
+            let samples = celt_audio.decode();
+            let channels = celt_audio.get_channels() as u16;
+            let sample_rate = celt_audio.get_sample_rate() as u32;
+
+            let wav_encoder = WavEncoder::new(&samples, channels, sample_rate);
+            wav_encoder.encode_to_file(&self.output_path);
+        }
 
         print!("Wrote output to \"{}\"", &self.output_path);
     }
